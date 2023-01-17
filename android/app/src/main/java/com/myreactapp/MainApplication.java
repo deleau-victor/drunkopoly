5e71c77916189c3497fb066532bc5366241f1c34
deleau.victor.pro@gmail.com <deleau.victor.pro@gmail.com>
2023-01-18 00:36:34 +0100

    rebuilding

--- android/app/src/main/java/com/myreactapp/MainApplication.java
+++ android/app/src/main/java/com/myreactapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myproject;
+package com.myreactapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -12,7 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import com.myproject.newarchitecture.MainApplicationReactNativeHost;
+import com.myreactapp.newarchitecture.MainApplicationReactNativeHost;
 
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
@@ -88,7 +88,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.myproject.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.myreactapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- android/app/src/main/java/com/myreactapp/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/myreactapp/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.myproject.newarchitecture.components;
+package com.myreactapp.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
