8f585dacdf5000c88f181943b90f64b559247239
deleau.victor.pro@gmail.com <deleau.victor.pro@gmail.com>
2023-01-18 15:50:31 +0100

    rebuilding

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -4,24 +4,36 @@ import { AspectRatio, Box, Button, Image, Text, View } from "native-base"
 type HomeProps = {}
 
 const Home: FC<HomeProps> = ({}) => {
-   return (
-       <View>
-            <Box 
-                style={{transform: [{ rotate: '-15deg' }]}}
-            >
-                <Image
-                    source={require('../assets/images/logo.png')}
-                    resizeMode="contain"
-                    width="80%"
-                    alt="logo"
-                    background='amber.100'
-                />
-                <Text color="primary.white" position="absolute" bottom={0} left='0' fontWeight="bold" fontSize="xl">Le Monopoly des soirées</Text>
-            </Box>
-           <Button width="60%" marginX='auto' _text={{fontWeight:'bold', fontSize:'2xl'}} background='primary.red' onPress={() => console.log("hello world")}>Jouer</Button>
-       </View>
-   )
+	return (
+		<View>
+			<Box style={{ transform: [{ rotate: "-15deg" }] }}>
+				<Image
+					source={require("../assets/images/logo.png")}
+					resizeMode='contain'
+					width='80%'
+					alt='logo'
+					background='amber.100'
+				/>
+				<Text
+					color='primary.white'
+					position='absolute'
+					bottom={0}
+					left='0'
+					fontWeight='bold'
+					fontSize='xl'>
+					Le Monopoly des soirées
+				</Text>
+			</Box>
+			<Button
+				width='60%'
+				marginX='auto'
+				_text={{ fontWeight: "bold", fontSize: "2xl" }}
+				background='primary.red'
+				onPress={() => console.log("hello world")}>
+				Jouer
+			</Button>
+		</View>
+	)
 }
 
 export default Home
-
