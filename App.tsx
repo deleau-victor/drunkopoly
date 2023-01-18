66f400b536ff6cdd0013da36d9461dfb12439568
deleau.victor.pro@gmail.com <deleau.victor.pro@gmail.com>
2023-01-18 17:00:43 +0100

    Store init

--- App.tsx
+++ App.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react"
 
 // Style
-import { NativeBaseProvider } from "native-base";
-import Theme from './Theme';
+import { NativeBaseProvider } from "native-base"
+import Theme from "./Theme"
 
 // Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 // Screens
-import Home from './screens/Home';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from "./screens/Home"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 
+// State management
+import { Provider } from "react-redux"
+import { store } from "./store/store"
 
 const Stack = createNativeStackNavigator()
 
-
 export default function App() {
-  return (
-    <NativeBaseProvider theme={Theme}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false, // hide the default header
-            }}
-            initialRouteName="Home" // set the initial route name
-          >
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </NativeBaseProvider>
-  );
+	return (
+		<Provider store={store}>
+			<NativeBaseProvider theme={Theme}>
+				<SafeAreaProvider>
+					<NavigationContainer>
+						<Stack.Navigator
+							screenOptions={{
+								headerShown: false, // hide the default header
+							}}
+							initialRouteName='Home' // set the initial route name
+						>
+							<Stack.Screen name='Home' component={Home} />
+						</Stack.Navigator>
+					</NavigationContainer>
+				</SafeAreaProvider>
+			</NativeBaseProvider>
+		</Provider>
+	)
 }
-
