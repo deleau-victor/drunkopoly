76c71d4c16894a9cd7105122e7a9f7740ef68af9
deleau.victor.pro@gmail.com <deleau.victor.pro@gmail.com>
2023-01-18 17:14:48 +0100

    Slice for player

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import playerReducer from "../slices/player.slice"
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		player: playerReducer,
+	},
 })
 
 export type RootState = ReturnType<typeof store.getState>
