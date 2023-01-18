d10e9631129a8ebf46eddd93e44c1e930270a208
DylanAumond <dylanaumond@gmail.com>
2023-01-18 10:23:54 +0100

    add style theme

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider,Text, View } from "native-base";
+import Theme from './Theme';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={Theme}>
+        <View flex={1} alignItems="center" justifyContent="center" bgColor="primary.blue">
+          <Text>sqd</Text>
+          <StatusBar style="auto" />
+        </View> 
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Theme.ts
@@ -0,0 +1,22 @@
+
+import { extendTheme } from 'native-base';
+
+const Theme = extendTheme({
+  colors:{
+    primary: {
+      red: "#E52528",
+      blue: "#0E396B",
+      grey: "#414141",
+    },
+    secondary:{
+      blue: "#069ABB",
+      orange: "#E4901E",
+      green: "#0B6E4F",
+      pink: "#E66395"
+    }
+  },
+  components: {
+  }
+})
+
+  export default Theme
