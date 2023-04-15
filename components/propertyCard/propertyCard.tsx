370177b7f5aa2d7a3bc7cd5217b086539303d270
DylanAumond <dylanaumond@gmail.com>
2023-04-15 21:29:45 +0200

    proprety card

---
+++ components/propertyCard/propertyCard.tsx
@@ -0,0 +1,46 @@
+// React
+import React, { FC, useEffect } from "react"
+
+// NativeBase
+import { Box, Column } from "native-base"
+
+// Components
+import PropertyHeaderCard from "./propertyHeaderCard"
+import PropertyBodyCard from "./propertyBodyCard"
+import PropertyFooterCard from "./propertyFooterCard"
+
+// Redux
+import { Player } from "../../store/types/player"
+
+type GameCardProps = {
+	close: () => void
+	propertyId: number | undefined // define the id of the property
+	owner: Player | undefined // define the owner's id of the property
+	context: "noOwner" | "isOwner" | "isLocator" | "isLooking"
+}
+
+const GameCard: FC<GameCardProps> = ({ close, propertyId, owner, context }) => {
+	return (
+		<Box
+			/*marginX='10%'*/
+			w='80%'
+			h='60%'
+			backgroundColor='primary.white'
+			opacity={100}>
+			{/* Header */}
+			<PropertyHeaderCard propertyId={propertyId!} />
+			{/* Body */}
+			<Column flex='1' padding='5' space='5'>
+				<PropertyBodyCard owner={owner} propertyId={propertyId!} />
+				{/* Action's button */}
+				<PropertyFooterCard
+					context={context}
+					propertyId={propertyId}
+					close={close}
+				/>
+			</Column>
+		</Box>
+	)
+}
+
+export default GameCard
